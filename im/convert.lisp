1f7b8636e14872812d2178b15c188c9db2283b1f
Matthew Kennedy <burnsidemk@gmail.com>
2018-12-27 15:28:22 -0600

    Conversion bindings

---
+++ im/convert.lisp
@@ -0,0 +1,17 @@
+(in-package #:im)
+
+(export '(convert-packing
+          convert-map-to-rgb))
+
+(defun convert-packing
+    (src-data-ptr dst-data-ptr
+     width height
+     src-depth dst-depth
+     data-type
+     src-packed-p)
+  ;; TODO
+  )
+
+(defun convert-map-to-rgb (data-ptr count depth packed-p palette)
+  ;; TODO
+  )
