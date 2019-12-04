48cd4e562d2870db8e492e3f42b41725b2c15a51
Matthew Kennedy <burnsidemk@gmail.com>
2019-12-04 13:50:15 -0600

    Add note about lua bindings

--- process/process.lisp
+++ process/process.lisp
@@ -6,3 +6,8 @@
   (:export #:counter-aborted))
 
 (in-package #:im-process)
+
+;;; Lua bindings have a nice template for how to wrap up the
+;;; processing functions nicely
+;;; 
+;;; http://svn.code.sf.net/p/imtoolkit/im/trunk/im/src/lua5/im_process.lua
